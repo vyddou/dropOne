25e15dc5adc84e281c3dc24930f78343c935df32
Frederic Lamagdelaine <f.lamag@free.fr>
2025-06-05 12:40:08 +0200

    modif alertes et navbar haut

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user! # Assurez-vous que cela existe si vous voulez une authentification par défaut
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    flash[:notice] = "Bon retour parmi nous #{resource.username} ! 🎵" if resource.respond_to?(:username)
+    super
+  end
+  
   protected
 
   def configure_permitted_parameters
