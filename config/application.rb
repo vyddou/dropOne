9b43846318f25044b493e4fd28b87f87e5c31af1
Ugo Vidberg <ugo.vidberg@gmail.com>
2025-06-05 17:43:12 +0200

    Configure autoload_paths for responders

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module DropOne
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_paths += %W(#{config.root}/app/responders)
 
     # Configuration for the application, engines, and railties goes here.
     #
