296d28099deac45ec015072e04e48d4643208ff4
Nathan LeBrusq <nathanlebrusqm@gmail.com>
2025-06-05 14:41:14 +0200

    search complet et d'autres features

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,15 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
+
+document.addEventListener('turbo:load', () => {
+  const input = document.getElementById('search-input');
+  const form = document.getElementById('search-form');
+
+  input.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault(); // empêche le comportement par défaut (si besoin)
+      form.requestSubmit();    // déclenche l'envoi du formulaire comme si tu cliquais sur le bouton
+    }
+  });
+});
