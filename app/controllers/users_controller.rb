1d5a95ebb204a43abdbbab3ab5f39f05aabf0bd9
Riboullet Romain <riboullet.romain@outlook.fr>
2025-06-05 17:30:57 +0200

    bouton like

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,32 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:show]
+
   def show
     @user = User.find(params[:id])
-    
+
     # Initialisation des variables
     @user_posts = @user.posts.order(created_at: :desc)
     @user_playlists = @user.playlists.order(created_at: :desc)
-    
+
     # Gestion des tracks likées
-    @like_playlist = @user.playlists.find_by(name: "Like") ######## s ou pas ??
+    @like_playlist = @user.playlists.find_by(name: "Like")  # (sans 's' à "Like")
     @user_tracks = @like_playlist ? @like_playlist.tracks.order(created_at: :desc) : []
-    
+
     # Variables pour les interactions
     @is_current_user = current_user == @user
-    @is_following = current_user.following.include?(@user) if user_signed_in? && !@is_current_user
+    @is_following = user_signed_in? && !@is_current_user ? current_user.following.include?(@user) : false
   end
 
   def follow
     @user = User.find(params[:id])
     current_user.following << @user
-    redirect_to user_path(@user), notice: "Vous suivez maintenant #{@user.email}"
+    redirect_to user_path(@user), notice: "Vous suivez maintenant #{@user.username}"
   end
 
   def unfollow
     @user = User.find(params[:id])
     current_user.following.delete(@user)
-    redirect_to user_path(@user), notice: "Vous ne suivez plus #{@user.email}"
+    redirect_to user_path(@user), notice: "Vous ne suivez plus #{@user.username}"
   end
 
   def update_description
@@ -40,4 +41,30 @@ class UsersController < ApplicationController
     end
   end
 
+  def activity
+    @user = User.find(params[:id])
+
+    # Activités de follow (abonnements)
+    follower_activities = @user.followers.map do |follower|
+      follow_record = follower.active_follows.find_by(second_user_id: @user.id)
+      {
+        type: :follow,
+        user: follower,
+        created_at: follow_record&.created_at || Time.zone.now
+      }
+    end
+
+    # Activités de like (votes sur les posts)
+    like_activities = Vote.where(post_id: @user.posts.ids).includes(:user, :post).map do |vote|
+      {
+        type: :like,
+        user: vote.user,
+        post: vote.post,
+        created_at: vote.created_at
+      }
+    end
+
+    # Fusion, tri descendant par date
+    @activity_feed = (follower_activities + like_activities).sort_by { |a| -a[:created_at].to_i }
+  end
 end
